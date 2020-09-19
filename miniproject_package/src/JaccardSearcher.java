803826666659e7787bf95d02035d929f9ea2ed9a
Nont4e <69662288+Nont4e@users.noreply.github.com>
2020-09-19 22:38:22 +0700

    Update JaccardSearcher.java

--- miniproject_package/src/JaccardSearcher.java
+++ miniproject_package/src/JaccardSearcher.java
@@ -26,7 +26,7 @@ public class JaccardSearcher extends Searcher{
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
 		List<String> query = Searcher.tokenize(queryString);
-		List<String> term;
+		List<String> Term;
 		List<SearchResult>results = new LinkedList<>();
 		double score = 0;
 		for (Document i : documents)
@@ -34,9 +34,15 @@ public class JaccardSearcher extends Searcher{
 			List<String> Union = new ArrayList<>(query);
 			List<String> Intersect = new ArrayList<>(query);
 
-			term = i.getTokens();
-			Union.addAll(term);
-			Intersect.retainAll(term);
+			Term = i.getTokens();
+			//Union
+			for(String t : Term){
+				if(Union.contains(t) == false)
+					Union.add(t);
+			}
+			//Intersect
+			Intersect.retainAll(Term);
+			//Do jaccard
 			double u_size = Union.size();
 			double in_size = Intersect.size();
 			if (u_size == 0 || in_size == 0)
@@ -50,3 +56,5 @@ public class JaccardSearcher extends Searcher{
 		return results.subList(0,k);
 		/***********************************************/
 	}
+
+}
