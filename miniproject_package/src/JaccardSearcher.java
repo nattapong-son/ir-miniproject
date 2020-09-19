f2264309b777b581c34439adb2af741661a24829
Nont4e <69662288+Nont4e@users.noreply.github.com>
2020-09-19 20:40:26 +0700

    Update JaccardSearcher.java

--- miniproject_package/src/JaccardSearcher.java
+++ miniproject_package/src/JaccardSearcher.java
@@ -2,22 +2,51 @@
 //Section: 
 //ID: 
 
-import java.util.List;
+import java.util.*;
 
 public class JaccardSearcher extends Searcher{
 
 	public JaccardSearcher(String docFilename) {
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
-		
+		//Tokenize a string (No duplicate)
+		List<String> word = new ArrayList<String>();
+		for (Document d : super.documents){
+			for (String i: d.getTokens())
+			{
+				if (word.contains(i) == false){
+					word.add(i);
+				}
+			}
+		}
 		/***********************************************/
 	}
 
 	@Override
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
-		return null;
+		List<String> query = Searcher.tokenize(queryString);
+		List<String> term;
+		List<SearchResult>results = new LinkedList<>();
+		double score = 0;
+		for (Document i : documents)
+		{
+			List<String> Union = new ArrayList<>(query);
+			List<String> Intersect = new ArrayList<>(query);
+
+			term = i.getTokens();
+			Union.addAll(term);
+			Intersect.retainAll(term);
+			double u_size = Union.size();
+			double in_size = Intersect.size();
+			if (u_size == 0 || in_size == 0)
+				score = 0;
+			else {
+				score = in_size / u_size;
+			}
+			results.add(new SearchResult(i,score));
+		}
+		Collections.sort(results);
+		return results.subList(0,k);
 		/***********************************************/
 	}
-
-}
